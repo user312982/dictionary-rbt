a15fd280c59ee78c741f603029075d7d6c3653df
user312982 <user312982@gmail.com>
2024-12-02 00:27:02 +0800

    delete tree.ts

--- dictionary/src/tree.ts
+++
@@ -1,6 +0,0 @@
-import ColorGimmick from "./components/ColorGimmick.vue";
-import { RedBlackTree } from "./red-black-tree";
-
-const tree = new RedBlackTree();
-
-tree.insert({ key: "hellowWorld", description: "" }, { value: "helloWorld", description: "" }       )
